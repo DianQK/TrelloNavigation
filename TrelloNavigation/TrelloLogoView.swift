6fe7ae1e6714f3c379e39a70dfe49fe33a9103ff
Qing <xiaoqing@dianqk.org>
2015-11-12 11:15:16 +0800

    细节调整

---
+++ TrelloNavigation/TrelloLogoView.swift
@@ -0,0 +1,21 @@
+//
+//  TrelloLogoView.swift
+//  TrelloNavigation
+//
+//  Created by 宋宋 on 15/11/12.
+//  Copyright © 2015年 Qing. All rights reserved.
+//
+
+import UIKit
+
+class TrelloLogoView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
